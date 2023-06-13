d6d9e88fbc4ff35e30f672d75c645e5ed9a0e3c0
Rajesh Sappa <xrsappa@LDO.NET>
2023-06-13 22:07:24 +0530

    added files

---
+++ 01-june/EmployeeDao.java
@@ -0,0 +1,99 @@
+package com.ded.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.ded.model.Employee;
+import com.ded.util.MyDbConnection;
+
+public class EmployeeDao {
+	
+	Connection con;
+	Statement stmt;
+	PreparedStatement ps;
+	ResultSet rs;
+	
+	public void insertEmployee(Employee emp) {
+		
+		con =MyDbConnection.getMyConnection();
+		try {
+			ps =con.prepareStatement("insert into DEDEMP values(?,?)");
+			ps.setInt(1, emp.getEno());
+			ps.setString(2, emp.getEname());
+			int noofrows =ps.executeUpdate();
+			System.out.println(noofrows + " got inserted into DB successfully!");
+		} catch (SQLException e) {
+		
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void showEmployee() {
+		con =MyDbConnection.getMyConnection();
+		try {
+			stmt =con.createStatement();
+			rs =stmt.executeQuery("select * from dedemp");
+			while(rs.next()) {
+				System.out.println(rs.getInt(1) + " -- " + rs.getString(2));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	public void updateEmployee(int eno, String ename) {
+		con = MyDbConnection.getMyConnection();
+		
+		try {
+			stmt = con.createStatement();
+			stmt.executeUpdate("update dedemp set ename='"+ename+"' where eno= "+eno);
+			System.out.println("employee got updated successfully!!");
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void deleteEmployee(int eno) {
+		con = MyDbConnection.getMyConnection();
+		
+		try {
+			stmt = con.createStatement();
+			stmt.executeUpdate("delete from dedemp where eno = "+eno);
+			System.out.println("Employee number '"+eno+"' deleted successfully!!");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void sortEmployee(int ch) {
+		con = MyDbConnection.getMyConnection();
+		
+		try {
+			stmt = con.createStatement();
+			if(ch == 1) {
+				rs = stmt.executeQuery("select * from dedemp order by ename");
+			}
+			else
+			{
+				rs = stmt.executeQuery("select * from dedemp order by eno");
+			}
+			while(rs.next()) {
+				System.out.println(rs.getInt(1) + " -- " + rs.getString(2));
+			}
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
---
+++ 01-june/MainClass.java
@@ -0,0 +1,121 @@
+package com.ded.view;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.IOException;
+import java.util.function.BiPredicate;
+
+import com.ded.controller.EmployeeController;
+
+import com.ded.controller.EmployeeInterface;
+
+import com.ded.Exception.UserNotFoundException;
+
+
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to EMS :)");
+		EmployeeController ec = new EmployeeController();
+		Scanner sc = new Scanner(System.in);
+		String ch=null;
+		
+		try {
+			String un = null;
+			String pwd = null;
+
+			InputStreamReader isr = new InputStreamReader(System.in);
+			BufferedReader br = new BufferedReader(isr);
+			System.out.println("Enter Username");
+			un = br.readLine();
+			System.out.println("Enter Password");
+			pwd = br.readLine();
+
+			if (un.equals("Moni") && pwd.equals("pass")) {
+				System.out.println("Welcome " + un);
+				System.out.println("Please wait.....loading");
+				Thread.sleep(5000);
+			
+		do {
+		System.out.println("Enter your choice");
+		System.out.println("1. Add Employee");
+		System.out.println("2. View Employee");
+		System.out.println("3. Serialize");
+		System.out.println("4. DeSerialize");
+		System.out.println("5. Update Employee");
+		System.out.println("6. Delete Employee");
+		System.out.println("7. Sort Employee");
+		int choice = sc.nextInt();
+		switch (choice) {
+		case 1: {
+			ec.addEmployee();
+			break;
+		}
+
+		case 2: {
+			ec.viewEmployee();
+			break; 
+		}
+		
+		case 3: { 
+			ec.SerialEg();
+			break;
+		}
+		
+		case 4:{
+			ec.DeSerialEg();
+			break;
+		}
+		
+		case 5:{
+			ec.updateEmployee();
+			break;
+		}
+			
+		case 6:{
+			ec.deleteEmployee();
+			ec.viewEmployee();
+			break;
+		}
+		case 7:{
+			ec.sortEmployee();
+			ec.viewEmployee();
+			break; 
+		}
+		
+		default: {
+			System.out.println("Enter a valid number");
+			break;
+		}
+		}
+		
+		System.out.println("Do u want to continue Y | y");
+		ch=sc.next();
+		
+		}while(ch.equals("Y") || ch.equals("y"));
+		System.out.println("Thanks for using our system.");
+		System.exit(0);
+			}
+			else {
+				throw new UserNotFoundException();
+			}
+
+		}
+
+		catch (UserNotFoundException unf) {
+			//System.out.println("From catch unf");
+			//System.out.println(unf);
+			unf.printStackTrace();
+		}
+
+		catch (Exception ae) {
+			System.out.println("IO");
+		} finally {
+			System.out.println("Finally .....");
+		}
+		System.out.println("Main Ends");
+	}
+
+}
